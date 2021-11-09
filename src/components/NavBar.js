33190227d2fa27d6ba4dc62e1a45fb9b78eed13a
milena salerno <milenacamila@outlook.es>
2021-11-08 23:53:00 -0300

    agregando navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import '../App.css'
+
+ export const NavBar = () => {
+    
+    return (
+    <div className="header">
+
+        <h1> Tiffany </h1>
+         
+         <nav>
+            <ul>
+                <li> Home </li>
+                <li> Productos </li>
+                <li>Contacto </li>
+            </ul>
+
+         </nav>
+    </div>
+    )
+}
+
